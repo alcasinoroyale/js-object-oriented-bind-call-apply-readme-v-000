e05dd5f4a386b557cfe629f392027f4d286b8e9d
Alex Zdatny <alexzdatny@gmail.com>
2018-12-19 21:06:44 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 // use this to copy code snippets or use your browser's console
-let sally = { name: 'Sally' };
-
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
 }
 
-greet.call(sally, 'Terry', 'George');
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+
+    matchInterests(event) {
+        return event.keywords.some(function(word) {
+            return this.interests.includes(word);
+        });
+    }
+}
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+
+billy.matchInterests(freeMusic);
