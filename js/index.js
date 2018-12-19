9f3c5fd04d3532b6c738a34ba79dcfbb8e7a6fdc
Alex Zdatny <alexzdatny@gmail.com>
2018-12-19 21:07:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -13,7 +13,9 @@ class User {
     }
 
     matchInterests(event) {
+        console.log("'this' is defined: ", this);
         return event.keywords.some(function(word) {
+            console.log("'this' is now undefined: ", this);
             return this.interests.includes(word);
         });
     }
